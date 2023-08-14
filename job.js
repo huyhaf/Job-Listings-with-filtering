aceac3515c4b06ec2a29d54a446ca410cb6d68a4
huyhaf <huyhn303@gmail.com>
2023-08-14 20:32:48 +0700

    edting JS

--- job.js
+++ job.js
@@ -1,43 +1,51 @@
 const jobelement=document.querySelectorAll('#Tec1');
-const skillelement=document.querySelectorAll('#Tec');
+const jobselement=document.querySelectorAll('#Job');
 
 
 const jobsearch=document.querySelector('#jobsearch');
 const skillsearch=document.querySelector('#skillsearch');
 
 let jobresult="",skillresult="";
+let searched = new Set();   // the variable record the job finded after the job searching
 
 jobsearch.addEventListener("keyup",() => {  // filtering the job
     jobresult=jobsearch.value.trim();
     jobresult=jobresult.toLowerCase(); // The searching result in lowercase
 
-    jobelement.forEach(function (e){    
-        let tmp1=e.innerHTML.toLowerCase();
+    for(var i =0 ;i<jobelement.length;i++){
+        let tmp1=jobelement[i].innerHTML.toLowerCase();
 
         if(tmp1.includes(jobresult)){
-            e.parentElement.style.display="block";
+            jobelement[i].parentElement.style.display="block";
+            searched.add(i);
         }
         else{
-            e.parentElement.style.display="none";
+            jobelement[i].parentElement.style.display="none";
         }
-
-    });
+    }
 });
 
 skillsearch.addEventListener("keyup", ()=>{
     skillresult=skillsearch.value.trim();
     skillresult=skillresult.toLowerCase(); // The searching result in lowercase
 
-    skillelement.forEach(function (e){    
-        let tmp2=e.innerHTML.toLowerCase();
+    for(var i=0;i<jobelement.length;i++){
+        if(searched.has(i)){ // if the tag appears after the job filtering
+            var len=jobelement[i].children.length;
+            var check=false;
 
-        if(tmp2.includes(skillresult)){
-            e.parentElement.style.display="block";
+            for(var j=0;j<len;j++){
+                let tmp2=jobelement[i].children[j].innerHTML;
+                tmp2=tmp2.toLowerCase();
+
+                if(tmp2.includes(skillresult)){
+                    check=true;
+                    jobelement[i].style.display="block";
+                }
+            }
+            if(!check){
+                jobelement[i].style.display="none";
+            }
         }
-        else{
-            e.parentElement.style.display="none";
-        }
-    });
+    };
 });
-
-
